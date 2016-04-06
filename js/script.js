3aebc2ac722e3323dd112cb888db72cb6d6e3575
Daniel Meana Llorián <Dani@MBPro.local>
2016-04-06 22:01:33 +0200

    Added images and gallery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+var images = {
+    aislamientostermicos: 8,
+    curiosidades: 18,
+    decorativas: 17,
+    exteriores: 28,
+    iglesias: 10,
+    imitaciones: 8,
+    industriales: 7,
+    interiores: 13,
+    panelesfibra: 4,
+    restaurantes: 6,
+    tejados: 2
+};
+
 var map;
 var imagesLoaded = false;
 var mapLoaded = false;
@@ -30,10 +44,33 @@ function loadMap() {
     });
 }
 
+function loadGallery(name, title) {
+    $(".gallery p.title").text(title);
+    var fotorama = $('#fotorama').fotorama().data('fotorama');
+    if (fotorama.size > 0) {
+        fotorama.show(0);
+    }
+    var imagesArray = [];
+    for (var i = 1; i <= images[name]; i++) {
+        var file = "img/works/" + name + "/" + i + ".jpg";
+        imagesArray.push({
+            img: file
+        });
+    }
+    console.log(imagesArray);
+    fotorama.load(imagesArray);
+}
+
 function setImagesToPortfolio() {
     var items = $("section#portfolio ul li");
     items.each(function() {
         $(this).css('background-image', 'url("img/works/' + $(this).data("name") + '.jpg")');
+        $(this).click(function(e) {
+            e.preventDefault();
+            var name = $(this).data("name");
+            var title = $(this).find("p:first").html();
+            loadGallery(name, title);
+        });
     });
 }
 
@@ -81,6 +118,21 @@ $(function() {
     $(".menu").click(function(e) {
         e.preventDefault();
         toogleMenu();
-    })
+    });
 
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) {
+            $("body").removeClass("modal-open");
+        }
+    });
+
+    $(".gallery .close").click(function(e) {
+        e.preventDefault();
+        $("body").removeClass("modal-open");
+    });
+
+    $("section#portfolio li").click(function(e) {
+        e.preventDefault();
+        $("body").addClass("modal-open");
+    });
 });
